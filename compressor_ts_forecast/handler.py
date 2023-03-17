bfb3fc2a03060487a894b9a6b163822575d1e794
Wei Wu <wei.wade.wu@gmail.com>
2023-03-16 18:15:30 -0700

    format fix

--- compressor_ts_forecast/handler.py
+++ compressor_ts_forecast/handler.py
@@ -48,39 +48,49 @@ def handle(client, data=None, secrets=None, function_call_info=None):
     Returns:
         response : response or result from the function
     """
-    data_set_id = client.data_sets.retrieve(external_id="cognite_replicator_test").id
+    compressor_ts_extid_list = [
+        "USA.ST.KONG.VIRT.005-CAE-5040A_Monitor_ActualPolytropicEfficiency-Numerical",
+        "USA.ST.KONG.VIRT.005-CAE-5040A_Monitor_ActualPolytropicHead",
+        "USA.ST.KONG.VIRT.005-CAE-5040A_Monitor_EfficiencyDeviation",
+    ]
+
+    data_set_id = 6870218523598358  # client.data_sets.retrieve(external_id="cognite_replicator_test").id
     column_names = ["Measurement"]
-    ts_exids = ["USA.ST.KONG.VIRT.005-CAE-5040A_Monitor_ActualPolytropicEfficiency"]
-    start_date = datetime.datetime(2023, 2, 1)
-    end_date = start_date + timedelta(days=30)
+    # ts_exids = ["USA.ST.KONG.VIRT.005-CAE-5040A_Monitor_ActualPolytropicEfficiency"]
+    start_date = datetime.datetime(2022, 6, 2)
+    end_date = start_date + timedelta(days=45)
+    for ts_exid in compressor_ts_extid_list:
+        print("Processing {}".format(ts_exid))
+        df = client.datapoints.retrieve_dataframe(
+            external_id=[ts_exid],
+            aggregates=["average"],
+            granularity="1h",
+            start=start_date,
+            end=end_date,
+            include_aggregate_name=False,
+        )
+        df.fillna(method="ffill", inplace=True)
+        df.columns = column_names
+        # remove outlier. TO DO optimization
+        df["Measurement"] = df["Measurement"].apply(lambda x: None if x == 0 else x)
+        df.reset_index(inplace=True)
+        # Forecast TS
+        fcst_df = ts_forecast(df)
 
-    df = client.datapoints.retrieve_dataframe(
-        external_id=ts_exids,
-        aggregates=["average"],
-        granularity="1h",
-        start=start_date,
-        end=end_date,
-        include_aggregate_name=False,
-    )
-    df.fillna(method="ffill", inplace=True)
-    df.columns = column_names
-    df.reset_index(inplace=True)
-    # Forecast TS
-    fcst_df = ts_forecast(df)
-
-    # Save the Results as time series
-    df.set_index(["index"], inplace=True)
-    create_and_save_time_series_data(client, df[["Measurement"]], f"{ts_exids[0]}_Actual", data_set_id=data_set_id)
-    create_and_save_time_series_data(
-        client, fcst_df[["yhat"]], f"{ts_exids[0]}_Forecast_Trend", data_set_id=data_set_id
-    )
-    create_and_save_time_series_data(
-        client, fcst_df[["yhat_lower"]], f"{ts_exids[0]}_Forecast_Lower", data_set_id=data_set_id
-    )
-    create_and_save_time_series_data(
-        client, fcst_df[["yhat_upper"]], f"{ts_exids[0]}_Forecast_Upper", data_set_id=data_set_id
-    )
-    create_and_save_time_series_data(client, fcst_df[["cap"]], f"{ts_exids[0]}_Forecast_Cap", data_set_id=data_set_id)
-    # Return the result as json
-    result = fcst_df[["yhat"]].to_json()
-    return result
+        # Save the Results as time series
+        df.set_index(["index"], inplace=True)
+        create_and_save_time_series_data(client, df[["Measurement"]], f"{ts_exid}_Actual", data_set_id=data_set_id)
+        create_and_save_time_series_data(
+            client, fcst_df[["yhat"]], f"{ts_exid}_Forecast_Trend", data_set_id=data_set_id
+        )
+        create_and_save_time_series_data(
+            client, fcst_df[["yhat_lower"]], f"{ts_exid}_Forecast_Lower", data_set_id=data_set_id
+        )
+        create_and_save_time_series_data(
+            client, fcst_df[["yhat_upper"]], f"{ts_exid}_Forecast_Upper", data_set_id=data_set_id
+        )
+        create_and_save_time_series_data(client, fcst_df[["cap"]], f"{ts_exid}_Forecast_Cap", data_set_id=data_set_id)
+        # Return the result as json
+        # result = fcst_df[["yhat"]].to_json()
+    print("processing is done")
+    return compressor_ts_extid_list
